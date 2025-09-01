7043557c52d09afdd76d560a2b51293be8d91356
vladislavcertopoloh-cmd <vladislavcertopoloh@gmail.com>
2025-09-01 11:02:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const sideMenu = document.getElementById('side-menu');
+
+    menuToggle.addEventListener('click', () => {
+        sideMenu.classList.toggle('active');
+    });
+});
